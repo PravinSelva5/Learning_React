664b46ba1307ea86bf623da641ca36e0a39e6f29
PravinSelva5 <selvarap@mcmaster.ca>
2020-09-15 15:30:41 -0400

    working on making an app with React

--- My_React_App/learning_react/src/App.js
+++ My_React_App/learning_react/src/App.js
@@ -3,11 +3,11 @@ import HelloWorld from './Components/HelloWorld'
 import Header from './Components/Header'
 import Footer from './Components/Footer'
 
+
 function App() {
   return (
     <div className="App">
       <Header/> 
-
       <HelloWorld name="Pravin"/>
       <Footer/>
     </div>
