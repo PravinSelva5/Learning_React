10a0768a28c5f1d0503feef6a1d12cc885e58c54
PravinSelva5 <selvarap@mcmaster.ca>
2020-09-17 01:40:21 -0400

    working through react tutorial

--- My_React_App/learning_react/src/Views/Product.js
+++ My_React_App/learning_react/src/Views/Product.js
@@ -1,26 +1,73 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
+import { useParams} from 'react-router-dom'
+import Loader from '../Components/Loader'
 
 function Product(){
-    const url ='https://5f60f04c90cf8d0016558837.mockapi.io/products/$%7Bid'
-    const [product, setProduct] = useState(null)
+    const { id } = useParams()
+    const url =`https://5f62e6ca363f0000162d7cd7.mockapi.io/products/${id}`
+    const [product, setProduct] = useState({
+        loading: false,
+        data: null,
+        error: false
+    })
 
-    useState( () => {
+    let content = null
+    
+    useEffect(() => {
+        setProduct({
+            loading: true,
+            data:null,
+            error:false
+        })
         
         axios.get(url).then(response => {
-        setProduct(response.data)
+            setProduct({
+                loading: false,
+                data: response.data,
+                error:false
+            })
+        })
+        .catch(error => {
+            setProduct({
+                loading: false,
+                data: null,
+                error: true
+            })
         })
 
     }, [url])
     
-
-    if(product){
-        return (
-            <div>
-                <h1>{product.name}</h1>
-            </div>
-        )
+    if(product.loading){
+        content = <Loader></Loader>
     }
+
+    if(product.data){
+        content = 
+            <div>
+                <h1 className="text-2xl font-bold mb-3">
+                    {product.data.name}
+                </h1>
+                <div>
+                    <img 
+                        src={product.data.images[0].imageUrl}
+                        alt={product.data.name}
+                    />
+                </div>
+                <div className="font-bold text-xl mb-3">
+                    ${product.data.price}
+                </div>
+                <div>
+                    {product.data.description}
+                </div>
+            </div>   
+    }
+
+    return (
+        <div>
+            {content}
+        </div>
+    )
 
 }
 
