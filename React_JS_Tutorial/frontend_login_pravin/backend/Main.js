17f34b9604e8e43ae7750fc06a1884f2975fbb50
PravinSelva5 <selvarap@mcmaster.ca>
2020-09-28 17:01:26 -0400

    working on backend for log-in app to apply to firstDash website

---
+++ React_JS_Tutorial/frontend_login_pravin/backend/Main.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const mysql = require('mysql');
+const session = require('express-session');
+const MySQLStore = require('express-mysql-session')(session);
+const Router = require('./Router');
+
+app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.json());
+
+
+// Database Connection 
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'myapp'
+});
+
+db.connect(function(err) {
+    
+    if (err) {
+        console.log('DB error');
+        throw err;
+        return false;
+    }
+
+});
---
+++ React_JS_Tutorial/frontend_login_pravin/backend/build/precache-manifest.23074a66af95328e5e1a9c9ef69a176c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc4d49b98d55ece79d0d985b76a5bee6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f7e812953084956f8931",
+    "url": "./static/css/main.879d690a.chunk.css"
+  },
+  {
+    "revision": "cd3ca47d8cdf70b9308b",
+    "url": "./static/js/2.91f5a9d3.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "./static/js/2.91f5a9d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7e812953084956f8931",
+    "url": "./static/js/main.660f2142.chunk.js"
+  },
+  {
+    "revision": "6b06bc07aae42b9b7e49",
+    "url": "./static/js/runtime-main.a4d1d089.js"
+  }
+]);
